9d8c022c14013c2f2b4bcce8ad4721bd3e9215d6
Yaroslav <my@my.ru>
2018-03-21 23:36:57 +0300

    init

---
+++ src/main/java/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+
+
+@RestController
+@RequestMapping("/test")
+class HelloResource {
+
+	@GetMapping
+	public String hello() {
+		return "Hello!!!";
+	}
+}
